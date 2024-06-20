13e43312c60f8daeb7762824d0a295399e104f97
Chetan <c27801j@gmail.com>
2024-06-20 16:15:02 +0530

    init commit

---
+++ common/src/index.ts
@@ -0,0 +1,35 @@
+import z from "zod";
+
+//signup
+export const signupInput = z.object({
+	email: z.string().email(),
+	password: z.string().min(6),
+	name: z.string().optional(),
+});
+
+//signin
+export const signinInput = z.object({
+	email: z.string().email(),
+	password: z.string().min(6),
+});
+
+//create blog
+export const createBlogInput = z.object({
+	title: z.string(),
+	content: z.string(),
+});
+
+//update blog
+export const updateBlogInput = z.object({
+	title: z.string(),
+	content: z.string(),
+	id: z.string(),
+});
+
+
+
+// type inference in zod (frontend)
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
