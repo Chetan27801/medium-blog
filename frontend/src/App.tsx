19fa1de9b8b9101cb50b5c483efcd5f997f34315
Chetan <c27801j@gmail.com>
2024-06-22 16:04:14 +0530

    changes in routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<Signin />} />
+				<Route path="/" element={<Signup />} />
 				<Route path="/signin" element={<Signin />} />
 				<Route path="/signup" element={<Signup />} />
 				<Route path="/blog" element={<Blog />} />
